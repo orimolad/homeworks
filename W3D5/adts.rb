7a2977b85c355f846e76ce50f5909befd78e9593
Ori Molad <orimolad17@gmail.com>
2020-01-02 22:10:48 -0800

    wrote the Queue class to follow FIFO

--- W3D5/adts.rb
+++ W3D5/adts.rb
@@ -16,3 +16,24 @@ class Stack
         underlying_array.last
     end
 end
+
+class Queue
+
+    attr_reader :underlying_array
+    def initialize
+        @underlying_array = []
+    end
+
+    def enqueue(el)
+        underlying_array.push(el)
+    end
+
+    def dequeue
+        underlying_array.shift
+    end
+
+    def peek
+        underlying_array.first
+    end
+
+end
