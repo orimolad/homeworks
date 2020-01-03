a23336f42466dae5eea48236c8bbdf235d3b2928
Ori Molad <orimolad17@gmail.com>
2020-01-02 21:54:48 -0800

    wrote a stack class to follow LIFO

---
+++ W3D5/adts.rb
@@ -0,0 +1,18 @@
+class Stack
+    attr_reader :underlying_array
+    def initialize
+        @underlying_array = []
+    end
+
+    def push(el)
+        underlying_array.push(el)
+    end
+
+    def pop
+        underlying_array.pop
+    end
+
+    def peek
+        underlying_array.last
+    end
+end
