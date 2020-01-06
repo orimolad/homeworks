9fec1a87eb232b0fb87ae954128e7cb704f9c683
Ori Molad <orimolad17@gmail.com>
2020-01-05 18:57:31 -0800

    defined graphnode class

---
+++ W4D1/graph_nodes.rb
@@ -0,0 +1,19 @@
+class GraphNode
+  attr_accessor :val, :neighbors
+
+  def initialize(val)
+    self.val = val
+    self.neighbors = []
+  end
+
+  def add_neighbor(node)
+    self.neighbors << node
+  end
+end
+
+end
+
+
+def bfs(starting_node, target_value)
+
+end
